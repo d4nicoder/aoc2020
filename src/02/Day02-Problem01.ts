3b83f9c095f139bae7487ef2e9c660d7ae0b6dec
Daniel Cañada García <danitetus@gmail.com>
2020-12-02 09:43:55 +0100

    Day 2 problem 1 solved

---
+++ src/02/Day02-Problem01.ts
@@ -0,0 +1,55 @@
+import fs from 'fs'
+import path from 'path'
+
+type IPasswordDetails = {
+    min: number;
+    max: number;
+    letter: string;
+    password: string;
+}
+
+const checkLetterTimes = (password: string, letter: string, min: number, max: number): boolean => {
+    const regex = new RegExp(letter, 'g')
+    const results = password.match(regex)
+    const foundedTimes: number = !results ? 0 : results.length
+    return (min <= foundedTimes && max >= foundedTimes)
+}
+
+const parseLine = (line: string): IPasswordDetails => {
+    const regex = /^(\d+)-(\d+)\s(\w):\s([\w]+)$/
+    const lineData = line.match(regex)
+    if (!lineData) {
+        throw new Error('Bad line format')
+    }
+    return {
+        min: parseInt(lineData[1], 10),
+        max: parseInt(lineData[2], 10),
+        letter: lineData[3],
+        password: lineData[4]
+    }
+}
+
+const start = async () => {
+    const inputFile = await fs.promises.readFile(path.join(__dirname, 'input.txt'))
+    const inputFileData = inputFile.toString().split('\n').filter((line) => line.trim() !== '')
+
+    const validPasswords: (IPasswordDetails | null)[] = inputFileData.map((line) => {
+        let data: IPasswordDetails | null
+        try {
+            data = parseLine(line)
+        } catch (e) {
+            console.error(`${line} password info invalid`)
+            data = null
+        }
+        return data
+    }).filter((lineData) => {
+        if (!lineData) {
+            return false
+        }
+        return checkLetterTimes(lineData.password, lineData.letter, lineData.min, lineData.max)
+    })
+
+    console.log(`There are ${validPasswords.length} valid passwords`)
+}
+
+start().catch(console.error)
